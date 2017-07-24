d777f69b026c3c4cdffdfcf1f4b841cc2803c921
tcjennings <toby@tobyjennings.com>
2017-07-24 04:02:48 +0000

    corrections to docker entrypoint script

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,11 +2,10 @@
 
 cd /stnp
 
-for plugin in "$ENABLED_PLUGINS"; do
+for plugin in $ENABLED_PLUGINS; do
  if [[ -f ./avail_plugins/${plugin}.js ]]; then
    cp ./avail_plugins/${plugin}.js ./plugins/
  fi
 done
 
-#$NODE ./server.js
-npm run start
+$NPM run start
