3d10dfc045342fe560ac7202f0ca1f69c24a16c3
Roberto Vega <roberto.vega87@gmail.com>
2017-11-14 23:07:29 -0600

    updated the sever and added Logger module

--- smartthings-nodeproxy/server.js
+++ smartthings-nodeproxy/server.js
@@ -21,107 +21,11 @@ var http = require('http');
 var app = express();
 var nconf = require('nconf');
 var fs = require('fs');
+var STL = require('smartthings-node-proxy-logger'); //STL - Smartthings Logger
 var chokidar = require('chokidar');
 nconf.file({ file: './config.json' });
 
-function fileWatchComponent(){
-
-//chose fs.watchFile() over fs.watch() incase the system is not windows or MacOS... Maybe its a RasberryPi
-
-  if(!logFileWatcher){
-    logFileWatcher = chokidar.watch(['general_log.txt','error_log.txt']);
-    logFileWatcher.on('change',(path,stats)=>{
-      if(nconf.get('maxLogSizeBytes') < stats.size)reduceLogSize(path);
-    })
-  }
-}
-
-function reduceLogSize (path){
-  const tempwritestream = fs.createWriteStream('temp_'+path); //create a temp file so we don't mess with the one we are replacing. it's currently being used by readstream.
-  const readstream = fs.createReadStream(path);
-  
-  readstream.on('readable',()=>{
-    let chunk;
-    let chunks;
-    let lognotice;
-    while (null !== (chunk = readstream.read(10))) {
-      chunks = chunks + chunk;
-      //check to see if the lenght of bytes that we have read so far is near the end of the file. If it is we will write to the temp file.
-      if(chunks.length > (nconf.get('maxLogSizeBytes') - 400) && !lognotice){
-        //put a line at the top of the file to show it was reduced
-        lognotice = 'The log was reduced in size and restarted a new one at this point. \r\n'
-        //convert the sring to a buffer
-        let stringBuff = Buffer.from(lognotice);
-        //write to the temp file. 
-        tempwritestream.write(stringBuff);
-        tempwritestream.write(chunk);
-      }else if(chunks.length > (nconf.get('maxLogSizeBytes') - 400 ) && chunk.length >= 10){ //we've already indicated the truncated location now write the remaining data from the old file.
-        tempwritestream.write(chunk)
-      }else if(chunk.length < 10){ // this is here because at the end of the stream you'll end up with less than 10 bytes of data and so we want to write remaining data to the stream.
-        tempwritestream.write(chunk)
-        tempwritestream.end();
-      }
-    }
-    //close the write stream so we can read from it.
-    
-  })
-
-  //listen for when we're done with the file that we want to replace, this will fire when the readable event has no more data.
-  readstream.on('close',()=>{
-    const writestream = fs.createWriteStream(path);
-    const tempreadstream = fs.createReadStream('temp_'+path);
-
-    tempreadstream.pipe(writestream);
-
-    writestream.on('close',()=>{
-      fs.unlink('temp_'+path) //delete the temporary file
-    });
-
-  })
-
-} 
-
-
-
-
-var logger = function(err,str) {
-  const mod = 'stnp';
-  const logTimeStamp = new Date().toISOString();
-  const opts = {
-      flags : 'a',
-      autoclose: true
-    }
-  if(err){
-    var errorString = 'looks like an error message wasn\'t written into the system for this error';
-    if(typeof err === 'object'){
-      //check if the error is coming from non existent file
-      if(err.code === 'ENOENT' && err.syscall && err.path){
-        errorString = "Error: "+ err.code + ": " +"no such file or directory, "+ err.syscall + " "+err.path;
-      }else{
-        //if not an error we were looking for still log the error object.
-        errorString = JSON.stringify(err);  
-      }
-    }else{
-      // We're assuming this is a custom string error that was coded in the original logger.
-      errorString = err;
-    }
-    writestream = fs.createWriteStream('error_log.txt',opts);
-    writestream.open();
-    writestream.write(logTimeStamp + ': [' + mod + '] ');
-    writestream.write(errorString);
-    writestream.end('\r\n');
-
-  }else{
-    writestream = fs.createWriteStream('general_log.txt',opts);
-    writestream.open();
-    writestream.write(logTimeStamp + ': [' + mod + '] '+str);
-    writestream.end('\r\n');
-  }
-
-  fileWatchComponent()
-  //hid the old logger since it was just loging to system and we wanted a file to send back to the end point
-  //console.log("[%s] [%s] %s", new Date().toISOString(), mod, str);
-}
+const logger = new STL(__dirname,'stnp');
 
 /**
  * Root route
@@ -134,13 +38,13 @@ app.get('/logs',function(req,res){
   const readstream = fs.createReadStream('general_log.txt',{autoclose:true});
   
   readstream.on('error',(err)=>{
-    logger(err);
+    logger.log(err);
     res.status(404).send("general log file not found return to URL / and try again.").end()
     const writestream = fs.createWriteStream('general_log.txt',{autoclose:true})
   })
 
   readstream.on('readable',()=>{
-    logger(null,"Logs were accessed")
+    logger.log(null,"Logs were accessed")
     
   })
   readstream.pipe(res);
@@ -150,12 +54,12 @@ app.get('/logs',function(req,res){
 app.get('/errors',function(req,res){
   const readstream = fs.createReadStream('error_log.txt',{autoclose:true});
   readstream.on('error',(err)=>{
-    logger(err);
+    logger.log(err);
     res.status(404).send("error log file not found return to URL / and try again.").end()
     const writestream = fs.createWriteStream('error_log.txt',{autoclose:true})
   })
   readstream.on('readable',()=>{
-    logger(null,"Error Logs were accessed")
+    logger.log(null,"Error Logs were accessed")
   })
      readstream.pipe(res);
 })
@@ -168,12 +72,12 @@ app.get('/favicon.ico',function(req,res){
  * Enforce basic authentication route; verify that HTTP.HEADERS['stnp-auth'] == CONFIG['authCode']
  */
 app.use(function (req, res, next) {
-  logger(req.ip+' '+req.method+' '+req.url);
+  logger.log(req.ip+' '+req.method+' '+req.url);
 
   var headers = req.headers;
   if (!headers['stnp-auth'] ||
     headers['stnp-auth'] != nconf.get('authCode')) {
-    logger('Authentication error');
+    logger.log('Authentication error');
     res.status(500).json({ error: 'Authentication error' });
     return;
   }
@@ -191,7 +95,7 @@ app.get('/subscribe/:host', function (req, res) {
   nconf.set('notify:port', parts[1]);
   nconf.save(function (err) {
     if (err) {
-      logger('Configuration error: '+err.message);
+      logger.log('Configuration error: '+err.message);
       res.status(500).json({ error: 'Configuration error: '+err.message });
       return;
     }
@@ -203,7 +107,7 @@ app.get('/subscribe/:host', function (req, res) {
  * Startup
  */
 var server = app.listen(nconf.get('port') || 8080, function () {
-  logger(null,'SmartThings Node Proxy listening at http://'+server.address().address+':'+server.address().port);
+  logger.log(null,'SmartThings Node Proxy listening at http://'+server.address().address+':'+server.address().port);
 });
 
 /**
@@ -217,10 +121,10 @@ fs.readdir('./plugins', function(err, files) {
     .forEach(function(file) {
       var plugin = file.split(".")[0];
       app.use('/plugins/'+plugin, require('./plugins/'+plugin)(function(data){notify(plugin,data);}));
-      logger(null,'Loaded plugin: '+plugin);
+      logger.log(null,'Loaded plugin: '+plugin);
     });
   } else {
-    logger(err);
+    logger.log(err);
   }
 });
 
@@ -232,7 +136,7 @@ fs.readdir('./plugins', function(err, files) {
 var notify = function(plugin, data) {
   if (!nconf.get('notify:address') || nconf.get('notify:address').length == 0 ||
     !nconf.get('notify:port') || nconf.get('notify:port') == 0) {
-    logger("Notify server address and port not set!");
+    logger.log("Notify server address and port not set!");
     return;
   }
 
@@ -250,7 +154,7 @@ var notify = function(plugin, data) {
 
   var req = http.request(opts);
   req.on('error', function(err, req, res) {
-    logger("Notify error: "+err);
+    logger.log("Notify error: "+err);
   });
   req.write(data);
   req.end();
